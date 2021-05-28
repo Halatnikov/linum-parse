2a17a156d585f316ba1ec2e708ab63de855b7dfd
Halatnikov <halatnikovpav@yandex.ru>
2021-05-29 01:02:53 +0700

    make pretty

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
+// Edited example Parse server for Linum Framework
 
+// Initialize
 const express = require('express');
 const app = express();
 
+// Pass server configuration
 const ParseServer = require('parse-server').ParseServer;
 const api = new ParseServer({
-  
-  cloud: __dirname + '/cloud/main.js',
+  // "unchangeable" values, probably should be environment values, but maybe later
+  cloud: __dirname + '/cloud/main.js', // Cloud code directory
   liveQuery: {
-    classNames: ['Posts', 'Comments'],
+    classNames: ['Posts', 'Comments'], // LiveQuery placeholder setup
   },
-  
+  // CONSTANTS
+  // app basic info
   appId: process.env.APP_ID,
   appName: process.env.APP_NAME,
   serverURL: process.env.SERVER_URL,
-  
+  // database url, the thing that it all works on
   databaseURI: process.env.DATABASE_URI,
-  
+  // keys for API access
   masterKey: process.env.MASTER_KEY,
   readOnlyMasterKey: process.env.READ_ONLY_MASTER_KEY,
   clientKey: process.env.CLIENT_KEY,
@@ -25,40 +27,41 @@ const api = new ParseServer({
   fileKey: process.env.FILE_KEY,
   restAPIKey: process.env.REST_API_KEY,
   javascriptKey: process.env.JAVASCRIPT_KEY,
+  // PROPERTIES
+  allowCustomObjectId: process.env.ALLOW_CUSTOM_OBJECT_ID || "true",
+  preserveFileName: process.env.PRESERVE_FILE_NAME || "true",
   
-  allowCustomObjectId: process.env.ALLOW_CUSTOM_OBJECT_ID,
-  preserveFileName: process.env.PRESERVE_FILE_NAME,
+  sessionLength: process.env.SESSION_LENGTH || "300",
   
-  sessionLength: process.env.SESSION_LENGTH,
-  
-  verbose: process.env.VERBOSE
+  logLevel: process.env.LOG_LEVEL || "INFO"
 });
 
+// Allow cross-origin requests
 const cors = require('cors');
 app.use(cors());
 
+// Weird thing where you need to listen for the port which Heroku gave you, otherwise server would crash
 const port = process.env.PORT
 const httpServer = require('http').createServer(app);
   httpServer.listen(port, function () {
-    console.log('parse-server-example running on port ' + port + '.');
+    console.log( process.env.APP_NAME + 'is running on port ' + port);
   });
 
-// This will enable the Live Query real-time server
+// Enable Live Query server (currently does nothing)
 ParseServer.createLiveQueryServer(httpServer);
 
-const path = require('path');
-
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
-
-// Serve the Parse API on the /parse URL prefix
+// Set the main path for Parse API
 const mountPath = process.env.PARSE_MOUNT
 app.use(mountPath, api);
 
-// Parse Server plays nicely with the rest of your web routes
+// other server path shenanigans
+const path = require('path');
+// display something when you're trying to access anything other than API
 app.get('/', function (req, res) {
   res.status(200).send('Nothing to see here?');
 });
+// static assets like pictures should go in /public
+app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // reference for later
 /* app.get('/test', function (req, res) {
