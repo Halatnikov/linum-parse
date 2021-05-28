75c149473a1493e2fef31e5db839bf962667db32
Halatnikov <43994658+Halatnikov@users.noreply.github.com>
2021-05-28 15:40:37 +0700

    add sessionLength

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const config = {
   preserveFileName: process.env.PRESERVE_FILE_NAME || '',
   readOnlyMasterKey: process.env.READ_ONLY_MASTER_KEY || '',
   restAPIKey: process.env.REST_API_KEY || '',
+  sessionLength: process.env.SESSION_LENGTH || '',
 };
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
